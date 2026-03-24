e96f983e4ab1bb9d72fd9f8e9bdf93dd73a7f6cf
Dustin <redhotblood1010@gmail.com>
2026-03-24 12:07:31 -0400

    g

--- animationM8_DL/ViewController.swift
+++ animationM8_DL/ViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var boxView: UIView!
+    
+    @IBAction func moveButton(_ sender: Any)
+    {
+        UIView.animate(withDuration: 5, animations: {
+            self.boxView.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
+        }, completion: {(finsihed: Bool) -> Void in print("Finished")})
+        
+        UIView.animate(withDuration: 5, animations: {
+            self.boxView.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
+        }, completion: {(finsihed: Bool) -> Void in print("Finished")})
+        
+        UIView.animate(withDuration: 5, animations: {
+            self.boxView.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
+        }, completion: { _ in print("Finished")})
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
